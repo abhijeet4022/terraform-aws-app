195712c1327965453bdc67fda5c5426c0f59c465
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-30 08:13:36 +0530

    Project Infra

--- main.tf
+++ main.tf
@@ -89,12 +89,14 @@ resource "aws_lb_target_group" "main" {
   target_type = "instance"
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
+  tags        = var.tags
 }
 
 # Create the Listener rule. Route the traffic to respective TG based on hostname
 resource "aws_lb_listener_rule" "main" {
   listener_arn = var.private_listener_arn
   priority     = var.lb_priority
+  tags         = { Name = "${var.component}-rule" }
 
   action {
     type             = "forward"
@@ -127,6 +129,7 @@ resource "aws_lb_target_group" "public" {
   target_type = "ip"
   protocol    = "HTTP"
   vpc_id      = var.default_vpc_id # This TG is part of Public LB.
+  tags        = var.tags
   #  health_check {
   #    enabled             = true
   #    healthy_threshold   = 2
