5503846b32691907f0edf4af678cecffdf59d7ec
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-30 16:19:51 +0530

    Project App

--- userdata.sh
+++ userdata.sh
@@ -4,4 +4,4 @@
 sudo yum install ansible -y &>> /opt/userdata.log
 
 # Run ansible-pull to pull the playbook and redirect both stdout and stderr to userdata.log
-ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible.git main.yml -e component=${component} &>> /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible-deployment.git main.yml -e component=${component} &>> /opt/userdata.log
