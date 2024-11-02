793fd6fd7153e4ed8f4661c84d0a819372f6e7db
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-02 16:25:18 +0530

    Project APP

--- main.tf
+++ main.tf
@@ -94,6 +94,12 @@ resource "aws_autoscaling_group" "main" {
     value               = local.name_prefix
     propagate_at_launch = true
   }
+
+  tag {
+    key                 = "Monitor"
+    value               = "yes"
+    propagate_at_launch = true
+  }
 }
 
 
