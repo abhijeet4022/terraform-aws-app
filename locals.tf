0ec898cabe9d4668fbbbc7c48a4381f9f855b810
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-11-21 14:14:12 +0530

    Get Folder Details

--- locals.tf
+++ locals.tf
@@ -2,8 +2,6 @@ locals {
   tags             = merge(var.tags, { module-name = "app" }, { env = var.env })
   name_prefix      = "${var.env}-${var.component}"
   parameters       = concat(var.parameters, [var.component])
-  policy_resources = concat([
-    for i in local.parameters :"arn:aws:ssm:us-east-1:060795929502:parameter/${i}.${var.env}.*"
-  ], var.kms_key_id)
+  policy_resources = concat([for i in local.parameters : "arn:aws:ssm:us-east-1:060795929502:parameter/${i}.${var.env}.*"
+  ], [var.kms_key_id])
 }
-
